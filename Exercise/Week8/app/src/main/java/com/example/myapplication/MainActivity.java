248a2e86d0614368d968eb691f507ebcac07703a
kyle-luo <tsjlky@gmail.com>
2019-10-23 15:54:22 -0400

    new

--- Exercise/Week8/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Exercise/Week8/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.more_layouts);
+        setContentView(R.layout.exercise);
     }
 }
